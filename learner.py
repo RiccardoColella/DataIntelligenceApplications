a2cbfa64b64e44898d38c1d6768eb858deda2605
terruzz <terruzzi.riccardo1997@gmail.com>
2021-05-01 12:13:13 +0200

    lol

--- learner.py
+++ learner.py
@@ -6,7 +6,8 @@ class Learner:
     def __init__(self, n_arms):
         self.n_arms = n_arms
         self.t = 0
-        self.rewards_per_arm = [[] for i in range(n_arms)]
+        self.rewards_per_arm = []
+        self.n_pulled_arms = [0] * n_arms
         self.collected_rewards = np.array([])
 
     def update_observations(self, pulled_arm, reward):
--- ucb1.py
+++ ucb1.py
@@ -7,6 +7,8 @@ class ucb1_learner(Learner):
         super().__init__(n_arms)
         self.empirical_means=np.zeros(n_arms)
         self.confidence=np.zeros(n_arms)
+        self.last30dayschoice=[]
+        self.delayedreward=[[]]
 
     def pull_arm(self):
         '''this function return the arm to be pulled at the next round'''
@@ -21,13 +23,28 @@ class ucb1_learner(Learner):
 
         return arm
 
-    def update_observations(self, pulled_arm, reward):
+    def update_observations(self, pulled_arm, reward, delayedr):
         '''this function updates the learner's parameter after a arm is pulled'''
+        if self.t<30:
+            self.last30dayschoice.append(pulled_arm)
+            self.delayedreward.append(delayedr)
+        else:
+            self.last30dayschoice=self.last30dayschoice.pop(0)
+            self.last30dayschoice.append(pulled_arm)
+            self.delayedreward=self.delayedreward.pop(0)
+            self.delayedreward.append(delayedr)
 
         self.t += 1
-        self.rewards_per_arm[pulled_arm].append(reward)
+        self.rewards_per_arm[pulled_arm]=self.rewards_per_arm[pulled_arm]+reward
         self.collected_rewards = np.append(self.collected_rewards, reward)
-        self.empirical_means[pulled_arm] = (self.empirical_means[pulled_arm]*(len(self.rewards_per_arm[pulled_arm])-1)+reward)/len(self.rewards_per_arm[pulled_arm])
+        self.n_pulled_arms[pulled_arm] += 1
+
+        for i in range(delayedreaward):
+            self.collected_rewards[-i-2] += self.delayedreward[-i-1][0]
+            self.rewards_per_arm[self.last30dayschoice[-i-1]] += self.delayedreward[-i-1][0]
+            self.delayedreward.pop([-i-1][0])
+
         for a in range(self.n_arms):
-            if len(self.rewards_per_arm[a])>0:
-                self.confidence[a] = (2*np.log(self.t)/len(self.rewards_per_arm[a]))**0.5
+            self.empirical_means[a]=self.rewards_per_arm[a]/self.n_pulled_arms[a]
+            if self.n_pulled_arms>0:
+                self.confidence[a] = (2*np.log(self.t)/self.n_pulled_arms)**0.5
