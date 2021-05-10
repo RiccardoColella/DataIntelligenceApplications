82569871f865fb85bd91803ebfe1a7051e977747
RiccardoColella <colella.git@outlook.com>
2021-05-10 22:15:32 +0200

    [TSGauss.py] La Fra ha fatto tutto pt2

--- tsgauss.py
+++ tsgauss.py
@@ -1,66 +1,70 @@
-from learner import Learner
+from learner import *
 import numpy as np
-import random
 
-class tsgauss_learner(Learner):
-    def __init__(self, n_arms):
-        super().__init__(n_arms)
-        self.empirical_means = np.zeros(n_arms)
-        self.dev = np.zeros(n_arms)
-        self.last30dayschoice = []
-        self.delayedreward = [[]]
-        self.rewards_per_arm = np.zeros(n_arms)
 
-    def pull_arm(self):
-        """ This function return the arm to be pulled at the next round
-        reminder: always call this function and then call update_observations
-        :return: the arm to be pulled at the next round
+class TSLearnerGauss(Learner):
+    """ Thomson Sampling Learner Class """
+
+    def __init__(self, n_arms, sigma=5):
+        """
+        Initialize the Thompson Sampling Learner class with number of arms, arms, sigma, expected mean.
+        :param n_arms:
         """
 
-        if self.t < self.n_arms:
-            arm = self.t
+        super().__init__(n_arms)  # supercharge init from the Learner
 
-        else:
-            sampled_values=[]
-            for i in range(self.n_arms):
-                sampled_values.append(int(np.random.normal(loc=self.empirical_means[i], scale=self.dev[i], size=1)))
-                arm=random.choice([i for i, j in enumerate(sampled_values) if j == max(sampled_values)])
+        # Assignments and Initializations
+        self.n_arms = n_arms
+        self.sigma = sigma
+        self.empirical_means = np.zeros(n_arms)
+        self.dev = np.zeros(n_arms)
+        self.last_30_days_choice = []
+        self.delayed_reward = [[]]
 
-        return arm
+    def pull_arm(self):
+        """
+        Pulls the current arm with the given budget and returns it.
+        :return: The index of the pulled arm
+        """
 
-    def update_observations(self, pulled_arm, reward, delayedr):
-        """ This function updates the learner's parameter after a arm is pulled
-        :param pulled_arm: The arm pulled
-        :param reward: The reward associated to the pulled arm
-        :param delayedr: The future 30 rewards (delayed rewards)
-        :return: none
+        sampled_values = [0] * self.n_arms
+        for i in range(self.n_arms):
+            sampled_values[i] = np.random.normal(loc=self.empirical_means[i], scale=self.dev[i], size=1)[0]
+
+        return np.argmax(sampled_values)
+
+    def update_observations(self, pulled_arm, reward, delayed_r):
+        """
+        Updates with the given pulled arm and reward.
+        :param pulled_arm: The chosen arm
+        :param reward: The assigned reward
+        :param delayed_r: The reward from the next 30 days
+        :return: NA
         """
 
         if self.t < 30:
-            self.last30dayschoice.append(pulled_arm)
-            self.delayedreward.append(delayedr)
+            self.last_30_days_choice.append(pulled_arm)
+            self.delayed_reward.append(delayed_r)
         else:
-            self.last30dayschoice.pop(0)
-            self.last30dayschoice.append(pulled_arm)
-            self.delayedreward.pop(0)
-            self.delayedreward.append(delayedr)
+            self.last_30_days_choice.pop(0)
+            self.last_30_days_choice.append(pulled_arm)
+            self.delayed_reward.pop(0)
+            self.delayed_reward.append(delayed_r)
 
         self.t += 1
-        self.rewards_per_arm[pulled_arm] = self.rewards_per_arm[pulled_arm] + reward
         self.collected_rewards = np.append(self.collected_rewards, reward)
+        self.rewards_per_arm[pulled_arm] = self.rewards_per_arm[pulled_arm] + reward
         self.n_pulled_arms[pulled_arm] += 1
 
-        if self.t>1:
-            for i in range(len(self.delayedreward)-1):
-                self.collected_rewards[-i-2] += self.delayedreward[-i-1][0]
-                self.rewards_per_arm[self.last30dayschoice[-i-1]] += self.delayedreward[-i-1][0]
-                self.delayedreward[-i-1].pop(0)
+        if self.t > 1:
+            for i in range(len(self.delayed_reward)):
+                self.collected_rewards[-i-1] += self.delayed_reward[-i - 1][0]
+                self.rewards_per_arm[self.last_30_days_choice[-i - 1]] += self.delayed_reward[-i - 1][0]
+                self.delayed_reward[-i - 1].pop(0)
         else:
-            #just remove the empty list at the beginning
-            self.delayedreward.pop(0)
+            # just remove the empty list at the beginning
+            self.delayed_reward.pop(0)
 
-        for a in range(self.n_arms):
-            #todo: per aggiornare i dati devo tenere conto della prior distribution e non solo dei dati empirici
-            if self.n_pulled_arms[a]>0:
-                self.empirical_means[a]=self.rewards_per_arm[a]/self.n_pulled_arms[a]
-                self.dev[a] = np.std(self.rewards_per_arm[a])
+        self.empirical_means[pulled_arm] = self.dev[pulled_arm] * ((self.empirical_means[pulled_arm] /
+                                                                self.dev[pulled_arm]) + reward / self.sigma ** 2)
+        self.dev[pulled_arm] = 1 / (1 / self.dev[pulled_arm] + 1 / self.sigma ** 2)
