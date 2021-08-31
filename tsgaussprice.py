76688a7f4ee68c3fb274c7613a49c93ba06ba5fd
roxpietro <roxpietro@gmail.com>
2021-09-01 00:13:09 +0200

    tsgauss sbagliato

--- tsgaussprice.py
+++ tsgaussprice.py
@@ -15,9 +15,9 @@ class TSLearnerGauss(Learner):
 
         # Assignments and Initializations
         self.n_arms = n_arms
-        self.sigma = 10
-        self.tau = [5] * n_arms
-        self.mu = [1800] * n_arms
+        self.sigma = 0.001
+        self.tau = [0] * n_arms
+        self.mu = [1000] * n_arms
         self.last30dayschoice = []
         self.delayedreward = []
         self.rewards_per_arm = np.zeros(n_arms)
