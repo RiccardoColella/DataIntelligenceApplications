0530e0cfbd4409d80a8f398e4ab807059548f62c
terruzz <terruzzi.riccardo1997@gmail.com>
2021-09-03 15:50:53 +0200

    daje

--- P3.py
+++ P3.py
@@ -43,8 +43,6 @@ bids = np.linspace(0.1, 1, num=10)
 bids, best_daily_price, best_possible_reward = get_best_bid_price_possible_reward(bids, prices)
 bids = [bids]
 
-print(bids, best_daily_price, best_possible_reward)
-
 # day of algorithm execution
 T = 395
 
