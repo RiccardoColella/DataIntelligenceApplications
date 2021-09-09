8608ea11c82189f56366982703a0d691c09f4ad3
terruzz <terruzzi.riccardo1997@gmail.com>
2021-09-09 09:10:42 +0200

    il mattino ha il ... in bocca

--- P1plot.py
+++ P1plot.py
@@ -1,7 +1,10 @@
 from matplotlib import pyplot
+from matplotlib import cm
 import os
 
 from environment import Environment
+
+from P1utilities import *
 
 env = Environment()
 
@@ -38,5 +41,24 @@ for i in range(1,4):
 pyplot.legend(class_list)
 pyplot.xlabel('Prices')
 pyplot.ylabel('Conversion rate')
+pyplot.xticks([prices[i] for i in range(1,len(prices),2)])
 pyplot.title(title)
 pyplot.savefig(os.path.join(plots_folder, title + '.png'))
+
+for classe in range(1,4):
+    title = 'Revenue function class ' + str(classe)
+    fig, ax = pyplot.subplots(subplot_kw={"projection": "3d"})
+    X, Y = np.meshgrid(bids, prices)
+    Z = np.array([[get_bid_and_price_revenue(bid, price, classe) for bid in bids] for price in prices])
+    if classe == 1:
+        ax.plot_surface(X, Y, Z, cmap = cm.Blues, linewidth=0, antialiased=False)
+    elif classe == 2:
+        ax.plot_surface(X, Y, Z, cmap = cm.Oranges, linewidth=0, antialiased=False)
+    else:
+        ax.plot_surface(X, Y, Z, cmap = cm.Greens, linewidth=0, antialiased=False)
+    ax.view_init(elev=30., azim=40)
+    pyplot.yticks([prices[i] for i in range(1,len(prices),2)])
+    pyplot.xlabel('Bids')
+    pyplot.ylabel('Prices')
+    pyplot.title(title)
+    pyplot.savefig(os.path.join(plots_folder, title + '.png'))
