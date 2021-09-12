a17b8fc23eb86904992cefef34cc520eb635b6de
terruzz <terruzzi.riccardo1997@gmail.com>
2021-09-12 16:20:25 +0200

    added latex printing

--- P1.py
+++ P1.py
@@ -1,10 +1,11 @@
 # the following 15 lines just add verbose option
 
 import argparse
-import pandas
 parser = argparse.ArgumentParser()
 parser.add_argument('-v', '--verbose', help="increase output verbosity", action="store_true")
+parser.add_argument('-l', '--latex', help="print revenue matrix in latex", action="store_true")
 verbose = parser.parse_args().verbose
+latex =  parser.parse_args().latex
 
 if verbose:
     def log(argument):
@@ -13,10 +14,21 @@ else:
     def log(argument):
         return
 
+if latex:
+    def printinlatex(argument):
+        print('dentro')
+        df = pandas.DataFrame(argument, prices, np.around(bids,decimals=1))
+        print( df.to_markdown(tablefmt="latex") )
+else:
+    def printinlatex(argument):
+        return
+
+
 # now the real code begins
 
 from environment import Environment
 import numpy as np
+import pandas
 
 from P1utilities import *
 
@@ -36,26 +48,30 @@ if __name__ == '__main__':
     revenuesmatrix = revenuesmatrix.reshape(bids.size, prices.size)
 
     revenuesmatrix_per_class = []
-    for i in range(1, 4):
-        revenuesmatrix_per_class.append(np.arange(bids.size * prices.size))
-        revenuesmatrix_per_class[-1] = revenuesmatrix_per_class[-1].reshape(bids.size, prices.size)
+    if verbose or latex:
+        for i in range(1, 4):
+            revenuesmatrix_per_class.append(np.arange(bids.size * prices.size))
+            revenuesmatrix_per_class[-1] = revenuesmatrix_per_class[-1].reshape(bids.size, prices.size)
 
     for i in range(bids.size):
         for j in range(prices.size):
             revenuesmatrix[i][j] = get_bid_and_price_revenue(bids[i], prices[j], 1) + get_bid_and_price_revenue(bids[i], prices[j], 2) +  get_bid_and_price_revenue(bids[i], prices[j], 3)
-            for k in range(1, 4):
-                revenuesmatrix_per_class[k-1][i][j] = get_bid_and_price_revenue(bids[i], prices[j], k)
+            if verbose or latex:
+                for k in range(1, 4):
+                    revenuesmatrix_per_class[k-1][i][j] = get_bid_and_price_revenue(bids[i], prices[j], k)
 
     best_bid = bids[np.unravel_index(np.argmax(revenuesmatrix), revenuesmatrix.shape)[0]]
     best_price = prices[np.unravel_index(np.argmax(revenuesmatrix), revenuesmatrix.shape)[1]]
 
     log("Revenue matrix per class:")
-    for i in range(0,3):
-        log('class' + str(i+1))
-        log(revenuesmatrix_per_class[i])
-    
+    if verbose or latex:
+        for i in range(0,3):
+            log('class' + str(i+1))
+            log(revenuesmatrix_per_class[i])
+            printinlatex(revenuesmatrix_per_class[i])
+
     log("Revenue matrix:")
-    df = pandas.DataFrame(revenuesmatrix, prices, np.around(bids,decimals=1))
-    log( df.to_markdown(tablefmt="latex") )
+    log(revenuesmatrix)
+    printinlatex(revenuesmatrix)
 
     print("The best joint bid and price strategy is (" + str(best_bid) + ', ' + str(best_price) + ')')
